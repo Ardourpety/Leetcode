6a494e48258bed1be2f81cc55d692a60605244fa
Ardourpety <43663362+Ardourpety@users.noreply.github.com>
2020-08-20 23:35:45 +0800

    Create Medium_Reorder_List.cpp

---
+++ Medium_Reorder_List.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head||!head->next) return;
+        
+        ListNode *p1=head, *p2=head->next;
+        
+
+        
+        while(p2 && p2->next){
+            p1=p1->next;
+            p2=p2->next->next;
+        }
+        
+        ListNode *head2=p1->next;
+        p1->next=NULL;
+        
+        p2=head2->next;
+        head2->next=NULL;
+        
+        
+        while(p2){
+            p1=p2->next;
+            p2->next=head2;
+            head2=p2;
+            p2=p1;
+        }
+        
+        
+        for(p1=head,p2=head2;p1;){
+            auto t = p1->next;
+            p1=p1->next=p2;
+            p2=t;
+        }
+        
+        
+    }
+};
